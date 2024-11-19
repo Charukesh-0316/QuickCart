f63bfe0273aba29b7fe9304349b3cb79cfd520da
Charukesh Naiknaware <naiknawarecharukesh@gmail.com>
2024-11-19 11:49:26 +0530

    New project created quickcart_01

---
+++ QuickCart-1/src/main/java/com/sunbeam/QuickCart1Application.java
@@ -0,0 +1,13 @@
+package com.sunbeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCart1Application.class, args);
+	}
+
+}
